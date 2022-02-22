978da86fda78fd5ba8b270aa7a5e8d66d7aeb052
Cristian Giudice <cristian@modelooperacional.com.br>
2022-02-22 20:53:50 -0300

    Initial

---
+++ Tarefa01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tarefa01.Soap;
+
+namespace Tarefa01
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            SOAPDemoSoapClient soapClient = new SOAPDemoSoapClient();
+
+            var response = await soapClient.AddIntegerAsync(15,25);
+
+            Console.WriteLine(response);
+
+            var person = await soapClient.FindPersonAsync("1");
+
+            Console.WriteLine(person.Name);
+
+            var divide = await soapClient.DivideIntegerAsync(100,10);
+
+            Console.WriteLine(divide);
+
+        }
+    }
+}
